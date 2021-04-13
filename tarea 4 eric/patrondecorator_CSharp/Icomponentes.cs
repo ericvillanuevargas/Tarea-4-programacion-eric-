b0debb5fa0751a1432e2947c50e4b2e04a610a23
Eric Villanueva <davidjr19110201@gmail.com>
2021-04-13 16:17:00 -0400

    este es el proyecto que funciona el otro tuvo problema

---
+++ tarea 4 eric/patrondecorator_CSharp/Icomponentes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tarea_4_eric
+{
+    interface Icomponentes
+    {
+        double costo();
+        string funciona();
+
+    }
+}
